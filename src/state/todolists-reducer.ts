fbca571f541941b4e413f64d458a1ba8805aeda3
Kirill Dolzhenkov <k.dolzhenkov@gmail.com>
2021-04-28 02:56:50 +0300

    added CHANGE-TODOLIST-TITLE and CHANGE-TODOLIST-FILTER action type to todolists-reducer + added same test to todolist-reducer.test + added types of actions to reducer & fixed action in tests

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,13 +1,36 @@
-import {TodoListType} from "../App";
+import {FilterValueType, TodoListType} from "../App";
 import {v1} from "uuid";
 
-
-type ActionType = {
+export type RemoveTodolistActionType ={
+    type: "REMOVE-TODOLIST"
+    id: string
+}
+export type AddTodolistActionType = {
+    type: "ADD-TODOLIST"
+    title: string
+}
+export type ChangeTodolistTitleActionType ={
+    type: "CHANGE-TODOLIST-TITLE"
+    title: string
+    id: string
+}
+export type ChangeTodolistFilterActionType ={
+    type: "CHANGE-TODOLIST-FILTER"
+    filter: FilterValueType
+    id: string
+}
+/*type ActionType = {
     type: string
     [key: string]: any
-}
+}*/
 
-export const todolistsReducer = (state: Array<TodoListType>, action: ActionType) => {
+export type ActionTypes =
+    RemoveTodolistActionType
+    | AddTodolistActionType
+    | ChangeTodolistTitleActionType
+    | ChangeTodolistFilterActionType
+
+export const todolistsReducer = (state: Array<TodoListType>, action: ActionTypes) => {
     switch (action.type) {
         case 'REMOVE-TODOLIST': {
             return state.filter(tl => tl.id != action.id)
@@ -19,8 +42,23 @@ export const todolistsReducer = (state: Array<TodoListType>, action: ActionType)
                 filter: "all"
             }]
         }
+        case 'CHANGE-TODOLIST-TITLE': {
+            let todoList = state.find(tl => tl.id === action.id );
+            if (todoList) {
+                todoList.title = action.title
+            }
+            return [...state]
+        }
+        case "CHANGE-TODOLIST-FILTER":{
+            let todoList = state.find(tl => tl.id === action.id );
+            if (todoList) {
+                todoList.filter = action.filter
+            }
+            return [...state]
+        }
         default:
             throw new Error("I don't understand this action type")
 
     }
 }
+
