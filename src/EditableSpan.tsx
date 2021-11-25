00d2017adfcccd083de56734be5b717d93dac2a6
Kirill Dolzhenkov <k.dolzhenkov@gmail.com>
2021-11-25 23:54:13 +0300

    added onKeyPress to edittablespan

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -11,19 +11,39 @@ export const EditableSpan = React.memo(function (props: EditableSpanPropsType) {
     let [editMode, setEditMode] = useState(false);
     let [title, setTitle] = useState(props.value);
 
-    const activateEditMode = () => {
+    /*const activateEditMode = () => {
         setEditMode(true);
         setTitle(props.value);
     }
     const activateViewMode = () => {
         setEditMode(false);
         props.onChange(title);
+    }*/
+    const changeEditMode = (value: boolean) => {
+        setEditMode(value);
+        if (value) {
+            setTitle(props.value);
+        } else {
+            props.onChange(title);
+        }
     }
+
     const changeTitle = (e: ChangeEvent<HTMLInputElement>) => {
         setTitle(e.currentTarget.value)
     }
+    const onKeyHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            changeEditMode(false);
+        }
+    }
 
     return editMode
-        ? <TextField value={title} onChange={changeTitle} autoFocus onBlur={activateViewMode}/>
-        : <span onDoubleClick={activateEditMode}>{props.value}</span>
+        ? <TextField
+            value={title}
+            onChange={changeTitle}
+            onBlur={()=>changeEditMode(false)}
+            onKeyPress={onKeyHandler}
+            autoFocus
+        />
+        : <span onDoubleClick={()=>changeEditMode(true)}>{props.value}</span>
 });
