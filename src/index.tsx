913bf6b1a919d4a40c76aebc9124546e93d0d083
Kirill Dolzhenkov <k.dolzhenkov@gmail.com>
2022-12-04 16:41:31 +0300

    added snapshot tests

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,8 @@ import AppWithRedux from './AppWithRedux';
 import {store} from './state/store';
 import {Provider} from 'react-redux';
 
+
+
 const container = document.getElementById('root') as HTMLElement
 const root = createRoot(container);
 root.render(
