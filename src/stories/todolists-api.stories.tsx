d2d1d48bed8876394a02a8d1243d22a076706408
Kirill Dolzhenkov <k.dolzhenkov@gmail.com>
2022-12-28 23:32:54 +0300

    added crud to storybook

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -23,12 +23,16 @@ export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
     let title = "Test"
     useEffect(() => {
-         let promise = axios.post(`https://social-network.samuraijs.com/api/1.1//todo-lists/`,
-             {title},
-             {withCredentials: true}
+         let promise = axios.post("https://social-network.samuraijs.com/api/1.1/todo-lists", {title},
+             {
+                 withCredentials: true,
+                 headers: {
+                     'API-KEY': '2f53ebc7-6e0c-44af-b6eb-a755cbe3639f'
+                 }
+             }
          )
          promise.then((response)=>{
-             debugger
+             setState(response.data)
          })
     }, [])
 
@@ -37,6 +41,16 @@ export const CreateTodolist = () => {
 export const DeleteTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        const todoId = "0e723aeb-ab74-4687-945e-7869af128f3b"
+        let promise = axios.delete(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todoId}`, {
+            withCredentials: true,
+            headers: {
+                'API-KEY': '2f53ebc7-6e0c-44af-b6eb-a755cbe3639f'
+            }
+        })
+        promise.then((response)=> {
+            setState(response.data)
+        })
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
@@ -44,6 +58,18 @@ export const DeleteTodolist = () => {
 export const UpdateTodolistTitle = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        const todoId = "218e812d-6278-48d5-bca4-fbb2fc9c8d63"
+        const title = "Vue2"
+        let promise = axios.put(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todoId}`,{title},
+            {
+                withCredentials: true,
+                headers: {
+                    'API-KEY': '2f53ebc7-6e0c-44af-b6eb-a755cbe3639f'
+                }
+            })
+        promise.then((response)=> {
+            setState(response.data)
+        })
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
